1c604d23661ecbffd8c28a7028c30d8154fd1063
iEclipse <atrang1994@gmail.com>
2015-05-23 00:49:00 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -86,6 +86,6 @@
         }
     }));
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+    $.getScript("https://github.com/iEclipse/basicBot-customization/blob/master/basicBot.js",
         extend);
 }).call(this);
