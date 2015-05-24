b223d95ab25260fcb6265d9de93ba2fb2455b8de
iEclipse <atrang1994@gmail.com>
2015-05-24 02:33:56 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2647,7 +2647,7 @@
             
             testCommand: {
                 command: 'test',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return
