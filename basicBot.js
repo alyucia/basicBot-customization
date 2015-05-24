49b383c89e9189c610fe32506a22aa089bdb4abf
iEclipse <atrang1994@gmail.com>
2015-05-24 02:18:27 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2655,7 +2655,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
                         $(".icon-population").click();
-                            $(".icon-banned").click()
+                            $(".icon-staff").click()
                     }
                 }
             },
