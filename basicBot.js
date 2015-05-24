62d6d4018449e765643295f2e65fdafeeecbdd28
iEclipse <atrang1994@gmail.com>
2015-05-23 18:57:18 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1630,26 +1630,7 @@
                     }
                 }
             },
-
-            blinfoCommand: {
-                command: 'blinfo',
-                rank: 'bouncer',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        var author = API.getMedia().author;
-                        var title = API.getMedia().title;
-                        var name = chat.un;
-                        var format = API.getMedia().format;
-                        var cid = API.getMedia().cid;
-                        var songid = format + ":" + cid;
-                        API.sendChat(subChat(basicBot.chat.blinfo, {name: name, author: author, title: title, songid: songid}));
-                    }
-                }
-            },
-
+            
             botnameCommand: {
                 command: 'botname',
                 rank: 'manager',
