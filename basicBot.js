cb72b91625ecfdc010cea95d9f14a86fcbd7861c
iEclipse <atrang1994@gmail.com>
2015-05-23 18:51:47 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2392,6 +2392,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void(0);
                     else {
+                        var id = API.getDJ().id;
                             if (chat.message.length === cmd.length) {
                                 API.sendChat(subChat(basicBot.chat.usedlockskip, {
                                     name: chat.un
