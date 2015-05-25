64e5ce3e6b3b7e365e7844df0a6f0c02f7f97c9b
iEclipse <atrang1994@gmail.com>
2015-05-25 11:27:11 -0700

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -2554,14 +2554,18 @@
 									}));
 								}, 2000);
 								setTimeout(function() {
-									var id1 = API.getWaitListPosition(basicBot.settings.fighter1.id)+1;
-									var id2 = API.getWaitListPosition(basicBot.settings.fighter2.id)+1;
+									var id1 = API.getWaitListPosition(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter1)
+										.id);
+									var id2 = API.getWaitListPosition(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter2)
+										.id);
 									if (id1 === -1 || id2 === -1) API.sendChat(basicBot.chat.notonwaitlist);
 									else if (id2 < id1) {
 										API.sendChat(basicBot.chat.swap);
-										basicBot.userUtilities.moveUser(basicBot.settings.fighter1.id, id2, false);
+										basicBot.userUtilities.moveUser(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter1)
+											.id, id2+1, false);
 										setTimeout(function() {
-											basicBot.userUtilities.moveUser(basicBot.settings.fighter2.id, id1, false);
+											basicBot.userUtilities.moveUser(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter2)
+												.id, id1+1, false);
 										}, 2000);
 									} else API.sendChat(subChat(basicBot.chat.unnecessaryswap, {
 										name: basicBot.settings.fighter1
@@ -2577,14 +2581,18 @@
 									}));
 								}, 2000);
 								setTimeout(function() {
-									var id1 = API.getWaitListPosition(basicBot.settings.fighter1.id)+1;
-									var id2 = API.getWaitListPosition(basicBot.settings.fighter2.id)+1;
+									var id1 = API.getWaitListPosition(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter1)
+										.id);
+									var id2 = API.getWaitListPosition(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter2)
+										.id);
 									if (id1 === -1 || id2 === -1) API.sendChat(basicBot.chat.notonwaitlist);
 									else if (id1 < id2) {
 										API.sendChat(basicBot.chat.swap);
-										basicBot.userUtilities.moveUser(basicBot.settings.fighter2.id, id1, false);
+										basicBot.userUtilities.moveUser(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter2)
+											.id, id1+1, false);
 										setTimeout(function() {
-											basicBot.userUtilities.moveUser(basicBot.settings.fighter1.id, id2, false);
+											basicBot.userUtilities.moveUser(basicBot.userUtilities.lookupUserName(basicBot.settings.fighter1)
+												.id, id2+1, false);
 										}, 2000);
 									} else API.sendChat(subChat(basicBot.chat.unnecessaryswap, {
 										name: basicBot.settings.fighter2
